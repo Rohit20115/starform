d3dbd4dd7250816b84f42fd3196e8a18ff7d13a7
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-14 13:31:21 +0530

    Add files via upload

---
+++ alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+
+        int n;
+        char c;
+
+        printf("enter the alphabets : ");
+        scanf("\n %d",&n);
+
+        switch(c)
+        {
+
+            
+                case 'a' :
+                printf("Apple");
+               // break;
+               // case 'B' :
+               // case 'b' :
+               // printf("Bat");
+               // break;
+
+               // default:
+               // printf("your choice is not matched");
+        }
+
+}
