3a97c865d9be68699b8df6dead7073011a454547
Rohit sahu <143478312+Rohit20115@users.noreply.github.com>
2024-01-21 13:47:59 +0530

    Add files via upload

---
+++ pattern_triangle_star.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+    
+     int i,j,n;
+  
+     printf("Enter the number:");
+     scanf("%d",&n);
+
+     for(i=1;i<=n;i++)
+     {
+        
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+     }
+
+}
